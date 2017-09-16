962e7aeb1723933340c73904eca64d93e3ead0db
americast <sayan.sinha@iitkgp.ac.in>
2017-09-16 08:37:30 +0530

    Avoid 100%

--- battery_notify.py
+++ battery_notify.py
@@ -1,15 +1,15 @@
 import os
 tmp = os.popen('upower -i $(upower -e | grep BAT) | grep --color=never -E "percentage"').read()
 # print tmp
-tmp = tmp[-4:-2]
+tmp = tmp[-5:-2]
 flag=True
 try:
-    a = int(tmp[0])
+    a = int(tmp[1])
     if a>=1:
         flag=False
 except:
     pass
-if int(tmp[1])<=9 and flag:
-  os.system('DISPLAY=:0 notify-send "Low battery" \"Only '+tmp[1]+'% remaining.\"')
+if int(tmp[2])<=9 and flag and tmp!='100':
+  os.system('DISPLAY=:0 notify-send "Low battery" \"Only '+tmp[2]+'% remaining.\"')
 # else: os.system('DISPLAY=:0 notify-send "Peace!" \"A whole lot i.e. '+tmp+'% remaining.\"')
 
